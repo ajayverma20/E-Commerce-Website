38076896b8aedd1b9101b12b5aa62f657383e36c
Ajay <ajay10verma1001@gmail.com>
2023-07-30 02:25:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+
+
+//61c854e7d6050ab502eb0bdd12f135f9d5c0ffab80cb429482083ad9a14bfac894a91f9ed1d98dfd256cc60c26489cce5048e5df67d7a83dcbb142066b6063b1d2d796f0ee4cbe3843fd7a62cbb8ad490db9a10ee95710c709ed816bc2cfb7095bf807ef5e9d2e00a9ffac57471aea8fa11e72772771d25697ae06f3abece7d4
